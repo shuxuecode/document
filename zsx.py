82c98c84f3843cf5d514d70fe679e7e7ba37adca
zhaoshuxue <zhaoshuxue@163.com>
2017-03-26 18:25:00 +0800

    commitByZsx

---
+++ zsx.py
@@ -0,0 +1,15 @@
+import os
+
+# os.chdir('E:/git_repo/springBoot') 
+
+os.system("git status")
+
+os.system("git pull")
+
+os.system("git add .")
+
+os.system("git commit -m commitByZsx")
+
+os.system("git push -u origin master")
+
+os.system("pause")

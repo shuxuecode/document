e7a5cecffaa7173d237f336b8f6a878e0adf6dba
zhaoshuxue <zhaoshuxue@qding.me>
2017-05-24 15:39:34 +0800

    commitByZsx

--- upload.sh
+++ upload.sh
@@ -6,15 +6,19 @@ sleep 2
 
 echo "hello world"
 
-
+echo "git status"
 git status
 
+echo "git pull"
 git pull
 
+echo "git add ."
 git add .
 
+echo "git commit -m commitByZsx"
 git commit -m commitByZsx
 
+echo "git push"
 git push
 
 read

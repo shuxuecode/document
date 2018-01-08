2e81f3ccefe055cdb5a70d4dd40b005e7693dcca
zhaoshuxue <zhaoshuxue@163.com>
2018-01-08 15:50:33 +0800

    commitByZsx

--- Python/code/2.py
+++ Python/code/2.py
@@ -3,11 +3,41 @@ a = b = c = 2
 
 print(a+b+c)
 
+# 也可以为多个对象指定多个变量,注意等号前后个数不一致会报错
 a, b, c = 1, 2, "runoob"
 
 print(a)
 print(b)
 print(c)
+
+# 内置的 type() 函数可以用来查询变量所指的对象类型
+print(type(a))
+print(type(c))
+
+# 数值运算
+
+# 加法
+print(1 + 2)
+
+# 减法
+print(11.4 - 3.3)
+
+# 乘法
+print(3 * 5)
+
+# 除法，得到一个浮点数
+print(3 / 2)
+
+# 除法，得到一个整数
+print(3 // 2)
+
+# 取余
+print(8 % 3)
+
+# 乘方
+print(2 ** 3)
+
+
 
 
 

de78a2e81abd04f38feb513a20b5b133e7d83782
zhaoshuxue <zhaoshuxue@163.com>
2018-05-24 09:38:29 +0800

    commitByZsx

--- Python/gif-to-mp4/loadGif.py
+++ Python/gif-to-mp4/loadGif.py
@@ -15,7 +15,8 @@ def main(gif_file, png_dir):
             current = img.tell()
             # print(current)
             png_num += 1
-            # img.save(png_dir+str(current)+'.png')
+            # 保存每一帧
+            img.save(png_dir+str(current)+'.png')
             img.seek(current+1)
 
     except:
@@ -24,9 +25,10 @@ def main(gif_file, png_dir):
 
 if __name__=='__main__':
     gif_file = 'D:/git/zhaoshuxue/document/Python/gif-to-mp4/test.gif'
-    gif_file = 'D:/log/000.gif'
+    gif_file = 'D:/git/oschina/准备发布的/3.gif'
     png_dir = gif_file[:-4] + '/'
-    # os.mkdir(png_dir)
+    # 创建同名文件夹
+    os.mkdir(png_dir)
     main(gif_file, png_dir)
     # 获取gif图片的信息
     img = Image.open(gif_file)
@@ -42,7 +44,7 @@ if __name__=='__main__':
     per = png_num / total_time
     print('mp4的帧率：', per)
 
-    cmd = 'D:/ProgramFilesFree/ffmpeg/bin/ffmpeg.exe -f image2 -r ' + str(per) + '  -i %d.png test.mp4'
+    cmd = 'D:/ProgramFilesFree/ffmpeg/bin/ffmpeg.exe -f image2 -r ' + str(per) + '  -i %d.png 1111123.mp4'
     # os.system('ffmpeg -f image2 -r 14.285  -i %d.png  ../test12.mp4')
     print(cmd)
     os.chdir(png_dir)

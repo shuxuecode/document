51bb37632cbdc89652bfcbc74cd368414dd0529d
zhaoshuxue <zhaoshuxue@163.com>
2019-05-09 14:29:32 +0800

    commitByZsx

--- Python/download下载图片.py
+++ Python/download下载图片.py
@@ -1,24 +1,21 @@
 
 import os
 import requests
-import random
 
 
 os.makedirs('D:/1/image/', exist_ok=True)
 
 imgUrls = [
-"https://wx3.sinaimg.cn/mw690/79a00895ly1fz2sduxi5jg208t08tnpp.gif",
-"https://wx2.sinaimg.cn/mw690/cfce7b59ly1g1ylbqr4mbj20ew0a574u.jpg",
-"https://wx4.sinaimg.cn/mw690/a8d43f7ely1g1yuquqiqoj20gi0i8tah.jpg",
-"https://wx1.sinaimg.cn/mw690/79a00895ly1g1oab03f8sg206x096hdt.gif",
-"https://wx4.sinaimg.cn/mw690/006q9cGPly1g1lopfbq21g30a80i8npg.gif",
-"https://wx1.sinaimg.cn/mw690/005G09gzly1g16tp9svicj30j70o0tcq.jpg",
-"https://wx1.sinaimg.cn/mw690/94525930gy1fwlth0zcd8g20hs0a0npd.gif",
-"https://wx4.sinaimg.cn/mw690/6e7905a7gy1g1fxvzadzfg20fs08wu14.gif",
-"https://wx1.sinaimg.cn/mw690/6e7905a7gy1g1fxuybs50g207w09jb29.gif",
-"https://wx3.sinaimg.cn/mw690/92ce84baly1g1u4pggbl7g20650aykjl.gif",
-"https://wx2.sinaimg.cn/mw690/79a00895ly1g1f6clge7sg204305ku0x.gif",
-"https://wx1.sinaimg.cn/mw690/79a00895gy1g1ubvkim6vg205c0744qp.gif" 
+"https://wx2.sinaimg.cn/mw690/7cc829d3ly1g1mz0wuvlpg208w053qv5.gif",
+"https://wx4.sinaimg.cn/mw690/006FzHykgy1g1ag3cyaogj30v919x1kx.jpg",
+"https://wx3.sinaimg.cn/mw690/7a722219gy1fzgdfpkmivj20hs0m7jt4.jpg",
+"https://wx3.sinaimg.cn/mw690/7a722219gy1fzgdfpkscmj20hs0m8tb8.jpg",
+"https://wx4.sinaimg.cn/mw690/7a722219gy1fzgdfph632j20hs0bmmyc.jpg",
+"https://wx2.sinaimg.cn/mw690/7a722219gy1fzgdfpfqtmj20hs0m5ac4.jpg",
+"https://wx2.sinaimg.cn/mw690/7a722219gy1fzgdfpb7m4j20hs0hswg4.jpg",
+"https://wx3.sinaimg.cn/mw690/7a722219gy1fzgdfpaylrj20hs0hstai.jpg",
+"https://wx4.sinaimg.cn/mw690/7a722219gy1fzgdfpdud3j20u011i0v4.jpg",
+"https://wx2.sinaimg.cn/mw690/7a722219gy1fzgdfpnh7sj20u011h7ae.jpg" 
 ]
 
 # for url in imgUrls:

ef6c7a7acc7e523c5ce5a3c93c425228417ea89e
zhaoshuxue <zhaoshuxue@163.com>
2019-08-29 23:35:54 +0800

    commitByZsx

--- Java/Code/Lucene/searchEverything/src/test/java/com/zsx/Demo.java
+++ Java/Code/Lucene/searchEverything/src/test/java/com/zsx/Demo.java
@@ -1,7 +1,44 @@
 package com.zsx;
 
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexReaderContext;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Demo {
 
 
+    public static void main(String[] args) {
+
+
+        Path path = null;
+        IndexWriterConfig config = null;
+        IndexWriter indexWriter = null;
+        try {
+            path = Paths.get("../test");
+            FSDirectory directory = FSDirectory.open(path);
+            config = new IndexWriterConfig();
+
+            indexWriter = new IndexWriter(directory, config);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        new IndexWriter("", null);
+
+
+//        IndexReader indexReader = new IndexReader("");
+
+//        IndexReaderContext context = new IndexReader().getContext();
+    }
 
 }

f867630d2e5d25ffec1d06a07ac02cc9f810f962
zhaoshuxue <zhaoshuxue@163.com>
2022-06-26 22:57:16 +0800

    :chart_with_upwards_trend: :rotating_light: :rewind:

--- Python/download下载图片.py
+++ Python/download下载图片.py
@@ -6,22 +6,7 @@ import requests
 os.makedirs('D:/1/image/', exist_ok=True)
 
 imgUrls = [
-"https://www.yuoimg.com/u/20190614/02064491.gif",
-"https://www.yuoimg.com/u/20190614/02064582.gif",
-"https://www.yuoimg.com/u/20190614/02064962.gif",
-"https://www.yuoimg.com/u/20190614/02065379.gif",
-"https://www.yuoimg.com/u/20190614/02065574.gif",
-"https://www.yuoimg.com/u/20190614/02065847.gif",
-"https://www.yuoimg.com/u/20190614/02070139.gif",
-"https://www.yuoimg.com/u/20190614/02070411.gif",
-"https://www.yuoimg.com/u/20190614/02070853.gif",
-"https://www.yuoimg.com/u/20190614/02071093.gif",
-"https://www.yuoimg.com/u/20190614/02071680.gif",
-"https://www.yuoimg.com/u/20190614/0207194.gif",
-"https://www.yuoimg.com/u/20190614/0207228.gif",
-"https://www.yuoimg.com/u/20190614/02072563.gif",
-"https://www.yuoimg.com/u/20190614/02072982.gif",
-"https://www.yuoimg.com/u/20190614/02073164.gif"
+"https://www.123.com/u/123/123.gif",
 ]
 
 # for url in imgUrls:

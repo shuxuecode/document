3ff19f690ec59ab74e7e2b5accf43b194adaf8f8
shuxuecode <zhaoshuxue@163.com>
2023-05-05 20:07:20 +0800

    :bulb: :art: :see_no_evil:

--- Python/code/excelTest.py
+++ Python/code/excelTest.py
@@ -6,9 +6,18 @@ sheet1 = wb[wb.sheetnames[0]]
 
 print("", sheet1['A2'].value)
 
+i = 0
+
 for row in sheet1.rows:
+    i += 1
+    print("row", i)
+
     for cel in row:
         value = cel.value
         print("", value)
+
+
+
+
 
 wb.close()

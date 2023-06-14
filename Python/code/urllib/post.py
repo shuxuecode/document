52632a3931a814e5f05f99b10629ca44fd0e4ce7
shuxuecode <zhaoshuxue@163.com>
2023-06-14 21:54:58 +0800

    :white_check_mark: :white_check_mark: :page_facing_up:

--- Python/code/urllib/post.py
+++ Python/code/urllib/post.py
@@ -1,12 +1,14 @@
 
-import urllib3
+# import urllib3
+from urllib3 import *
 
-http = urllib3.PoolManager()
-url = 'http://www.baidu.com/s'
+# http = urllib3.PoolManager()
+http = PoolManager(timeout=Timeout(connect=2.0, read=2.0))
+url = 'http://coolaf.com/tool/params'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
-response = http.request('POST', url, fields={'wd': '机器学习', 'l': 'CH'}, headers=headers)
+response = http.request('POST', url, fields={'s': 'stest', 'i': 'test'}, headers=headers)
 result = response.data.decode('UTF-8')
 print(result)
 

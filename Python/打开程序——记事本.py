80708fac24971bf1d4e72c1337e709370be003c7
zhaoshuxue <zhaoshuxue@163.com>
2023-08-09 23:47:21 +0800

    :see_no_evil: :fire: :hammer:

--- Python/打开程序——记事本.py
+++ Python/打开程序——记事本.py
@@ -1,7 +1,16 @@
 import os
 
-os.system('notepad')
+# os.system('notepad')
 
 
 # os.system('shutdown -r -t 10')
 # os.system('shutdown -a')
+
+print('仅仅打印日志')
+
+# with open('log.txt', 'w') as f:
+#     lines = ['a', 'b', 'c']
+#     f.writelines(lines)
+#     # f.flush()
+#     # f.close()
+
